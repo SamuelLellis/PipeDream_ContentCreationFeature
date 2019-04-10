5ce2d65f28e3d4fd8de072911dc68497884fb12c
Christian Mcborrough <christianmcborrough@gmail.com>
2019-04-10 18:06:30 -0400

    Post and get requests

--- finalProd.py
+++ finalProd.py
@@ -1,44 +1,11 @@
-# from flask import Flask, request, jsonify
-# from flask_restful import Resource, Api
+from flask import Flask, request, jsonify
 import mysql.connector
 from connect import connect
-# # from flask_sqlalchemy import SQLAlchemy
 import datetime
-# # import json
-#
+
 mydb = connect()
 mycursor = mydb.cursor()
-#
-# app = Flask(__name__)
-# api = Api(app)
-#
-# # class Content(Resource):
-# @app.route('/get', methods=['GET'])
-# def get(self, id): #Gets content from database
-#     mycursor.execute("select * from mydatabase")
-#     records = mycursor.fetchall()
-#     for row in records:
-#         if(row[0]== id):
-#             content = row[1]
-#             break
-#     return jsonify({'content':content})
-#
-# @app.route('/add', methods=['POST'])
-# def add(self, data):
-#     content = data.get_json()
-#     now = datetime.datetime.now()
-#
-#     mycursor.execute('''
-#         INSERT INTO localhost.mydatabase.mydatabase (id,content,author,data/time)
-#         VALUES
-#         (id,data,author,now
-#         ''')
-#     mydb.commit()
-#     return jsonify({'result':'Sucess'})
-#
-#     # def edit(self, id):
-#
-# api.add_resource(Content, '/content/<string:name>')
+
 
 from flask import Flask, request
 app = Flask(__name__)
@@ -56,4 +23,11 @@ def post_request():
              ({0},'{1}','{2}','{3}');
              '''.format(id, content, author, now))
     mydb.commit()
-    return 'clip'
+    return 'Successfully added contents'
+
+@app.route('/',methods=['GET'])
+def get_request():
+    id = request.args.get('id')
+    mycursor.execute("select * from mydatabase where id = {0}".format(id))
+    content = mycursor.fetchone()
+    return jsonify({'result':content[1]})
